8e92a011d18fea07fc109438bfe0eadca37cec4a
Adamsimsy <adamsimsy@gmail.com>
2019-09-08 06:47:43 +0000

    Added example project

---
+++ src/Xamarin-App-DevOps-Example/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Xamarin_App_DevOps_Example.Models;
+
+namespace Xamarin_App_DevOps_Example.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
